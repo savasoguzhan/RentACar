5976fc1fd5d91e739009f28ac2b3bc755141c0e8
Oğuzhan Savaş <mustafaoguzhan.savas@bilgeadamboost.com>
2024-05-09 13:49:59 +0300

    contact api consume

---
+++ WEB/RentACar.WebUI/Controllers/ContactController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RentACar.Dto.ContactDTOS;
+using System.Text;
+
+namespace RentACar.WebUI.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ContactController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(CreateContactDtos createContactDtos)
+        {
+            var client = _httpClientFactory.CreateClient();
+            createContactDtos.SendData=DateTime.Now;
+            var jsonData = JsonConvert.SerializeObject(createContactDtos);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8,"application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7047/api/Contact/", stringContent);
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index","Default");
+
+            }
+            return View();
+        }
+    }
+}
---
+++ WEB/RentACar.WebUI/ViewComponents/FooterAddressComponents/FooterAddressComponentPartial.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RentACar.Dto.FooterAddressDTOS;
+
+namespace RentACar.WebUI.ViewComponents.FooterAddressComponents
+{
+    public class FooterAddressComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public FooterAddressComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7047/api/FooterAddresses");
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultFooterAddressDtos>>(jsonData);
+                return View (values);
+            }
+            return View();
+        }
+    }
+}
