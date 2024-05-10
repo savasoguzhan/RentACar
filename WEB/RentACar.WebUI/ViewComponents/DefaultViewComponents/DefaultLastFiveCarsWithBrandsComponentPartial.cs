0cfc5ec81acf5c55e4ce04a14ece66072b507411
Oğuzhan Savaş <mustafaoguzhan.savas@bilgeadamboost.com>
2024-05-10 13:37:18 +0300

    Latst 5 cars List UI addedd

---
+++ WEB/RentACar.WebUI/ViewComponents/DefaultViewComponents/DefaultLastFiveCarsWithBrandsComponentPartial.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RentACar.Dto.CarDTOS;
+
+namespace RentACar.WebUI.ViewComponents.DefaultViewComponents
+{
+    public class DefaultLastFiveCarsWithBrandsComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public DefaultLastFiveCarsWithBrandsComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7047/api/Car/GetLastFiveCarWithBrand");
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultLastFiveCarsWithBrandsDtos>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
