013df63b7832461f62c35e04f40c1481b96986a5
Oğuzhan Savaş <mustafaoguzhan.savas@bilgeadamboost.com>
2024-05-11 12:58:54 +0300

    added service UI to default index page

--- WEB/RentACar.WebUI/Controllers/ServiceController.cs
+++ WEB/RentACar.WebUI/Controllers/ServiceController.cs
@@ -6,23 +6,10 @@ namespace RentACar.WebUI.Controllers
 {
     public class ServiceController : Controller
     {
-        private readonly IHttpClientFactory _httpClientFactory;
-
-        public ServiceController(IHttpClientFactory httpClientFactory)
+        
+        public  IActionResult Index()
         {
-            _httpClientFactory = httpClientFactory;
-        }
-        public async Task<IActionResult> Index()
-        {
-            var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync("https://localhost:7047/api/Services");
-            if(responseMessage.IsSuccessStatusCode)
-            {
-                var jsonData = await responseMessage.Content.ReadAsStringAsync();
-                var values = JsonConvert.DeserializeObject<List<ResultServiceDtos>>(jsonData);
-                return View(values);
-            }
-            return View();
+           return View();
         }
     }
 }
---
+++ WEB/RentACar.WebUI/ViewComponents/ServiceViewComponents/ServiceComponentPartial.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RentACar.Dto.ServiceDTOS;
+
+namespace RentACar.WebUI.ViewComponents.ServiceViewComponents
+{
+    public class ServiceComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ServiceComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7047/api/Services");
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultServiceDtos>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
